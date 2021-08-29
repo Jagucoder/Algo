7a315fd582f3fe1b88861a15130442f0fde10c78
Yiming Gui <yiming.gui@icloud.com>
2021-08-29 22:55:50 +0800

    add UnionFind

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,5 +1,6 @@
+#compile path should be root path, both for package and it's invoker.
+#packge declaration in binSearch then can use packageName.className to run.
 
-#packge in binSearch then can use packageName.className to run
-
+javac mytool/Stdin.java;
 javac binSearch/BinarySearch.java;
 java  binSearch.BinarySearch algs4-data/largeW.txt < algs4-data/largeT.txt
--- UnionFind/UnionFind.java
+++ UnionFind/UnionFind.java
@@ -1,1 +1,65 @@
+package UnionFind;
 
+import mytool.Stdin;
+
+public class UnionFind
+{
+    private int[] id;
+    private int count;
+    public UnionFind(int n)
+    {
+        count = n;
+        id = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            id[i] = i;
+        }
+    }
+    public int count()
+    {
+        return count;
+    }
+
+    public int find(int p)
+    {
+        return id[p];
+    }
+
+    public void union(int p, int q)
+    {
+        int pId = find(p);
+        int qId = find(q);
+
+        if (pId == qId) return;
+
+        for (int i = 0; i < id.length; i++)
+        {
+            if (id[i] == pId)
+            {
+                id[i] = qId;
+            }
+        }
+
+        count--;
+
+    }
+
+    public boolean connected(int p, int q)
+    {
+        return id[p] == id[q];
+    }
+    public static void main(String[] args)
+    {
+        int n = Stdin.readInt();
+        UnionFind uf = new UnionFind(n);
+        while(!Stdin.isEmpty())
+        {
+            int p = Stdin.readInt();
+            int q = Stdin.readInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            System.out.println(p + " " + q);
+        }
+        System.out.println(uf.count() + " components");
+    }
+}
